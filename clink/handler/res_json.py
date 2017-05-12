509e544b5c85b7a66e1d78dec0dcb3ce45699c26
Kevin Leptons <kevin.leptons@gmail.com>
2017-05-13 00:37:14 +0700

    save

--- clink/handler/res_json.py
+++ clink/handler/res_json.py
@@ -1,4 +1,21 @@
-import json
+'''
+SYNOPSIS
+
+    class ResJsonHandler
+
+DESCRIPTION
+
+    Encode object to JSON string. Use bson.json_util as encoder, output
+    is strange. For example:
+
+        o = {_id: ObjectId('5915e505e77989755d3cf4db')}
+
+    After encode, result is:
+
+        s = {"_id": {"$oid": "5915e505e77989755d3cf4db"}}
+'''
+
+from bson import json_util
 
 from ..iface import IPipeHandler
 from ..mime.type import MIME_JSON
@@ -11,4 +28,4 @@ class ResJsonHandler(IPipeHandler):
             return
         if res.body is None:
             return
-        res.body = json.dumps(res.body).encode(UTF_8)
+        res.body = json_util.dumps(res.body).encode(UTF_8)
--- clink/handler/send.py
+++ clink/handler/send.py
@@ -5,13 +5,15 @@ from ..iface import ISendHandler
 
 class SendHandler(ISendHandler):
     def handle(self, req, res, wsgi_send):
+        if res.body is not None and not isinstance(res.body, bytes):
+            raise WsgiResBodyError(res.body)
+
         header = [(k, v) for k, v in res.header.items()]
         if res.content_type is not None:
             header.append(('Content-Type', res.content_type))
+        wsgi_send(code_to_str(res.status), header)
+
         if res.body is None:
             return []
-        elif not isinstance(res.body, bytes):
-            raise WsgiResBodyError(res.body)
         else:
-            wsgi_send(code_to_str(res.status), header)
-            return [res.body]
+            return [res.body]        
